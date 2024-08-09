b5f7b876cc5dc664d842f6fbf556aea882ffb2f0
Aemal <aemal7@icloud.com>
2024-08-09 23:13:09 +0500

    updated and improved load todos

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
+const apiURL = 'https://jsonplaceholder.typicode.com/todos';
+
 const todoList = document.getElementById('todo-list');
 
 function loadTodos() {
-  fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  fetch(`${apiURL}?_limit=5`)
     .then((res) => res.json())
-    .then((todos) => loadTodosToDOM(todos));
-}
-
-function loadTodosToDOM(todos) {
-  todos.forEach((todo) => {
-    createToDoInDOM(todo);
-  });
+    .then((todos) => todos.forEach((todo) => createToDoInDOM(todo)));
 }
 
 function createToDoInDOM(todo) {
   const todoItem = document.createElement('div');
   todoItem.appendChild(document.createTextNode(todo.title));
+  todoItem.setAttribute('data-item', todo.id);
   todo.completed && todoItem.classList.add('done');
   todoList.appendChild(todoItem);
 }
